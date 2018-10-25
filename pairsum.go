f107a4a54575a69d76ef4b47270ea503b83b3bb4
totemteleko <totemteleko@gmail.com>
2018-10-25 17:07:09 -0400

    Add pairsum

---
+++ pairsum.go
@@ -0,0 +1,40 @@
+package algorithms
+
+import(
+	"sort"
+)
+
+// O(N) space, O(N) memory
+func FindPairHash(arr []int, sum int) (int, int){
+  found := make(map[int]int)
+  
+  for i:=0; i<len(arr); i++{
+    if v, ok := found[arr[i]]; ok {
+      return v,i
+    }
+    
+    found[sum - arr[i]] = i
+  }
+  return -1, -1
+}
+
+// O(NlogN) + O(N)
+func FindPairWithSort(arr []int, sum int) (int,int){
+  sort.Ints(arr)
+  
+  i, j := 0, len(arr) -1
+  for i < j {
+    complementary := sum - arr[j]
+    if arr[i] == complementary {
+      return i, j
+    }
+    
+    if arr[i] < complementary {
+      i++
+    }else{
+      j--
+    }
+  }
+  
+  return -1,-1
+}
---
+++ pairsum_test.go
@@ -0,0 +1,34 @@
+package algorithms_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/jszroberto/go-algorithms"
+)
+
+var _ = Describe("Pairsum", func() {
+  It("works", func(){
+    arr := []int{1,2,3}
+    
+    i, j := FindPairWithSort(arr,3)
+    Expect(i).To(Equal(0))
+    Expect(j).To(Equal(1))
+    
+    i, j = FindPairWithSort(arr,6)
+    Expect(i).To(Equal(-1))
+    Expect(j).To(Equal(-1))
+  })
+  
+  FIt("works", func(){
+    arr := []int{1,2,3}
+    
+    i, j := FindPairHash(arr,3)
+    Expect(i).To(Equal(0))
+    Expect(j).To(Equal(1))
+    
+    i, j = FindPairHash(arr,6)
+    Expect(i).To(Equal(-1))
+    Expect(j).To(Equal(-1))
+  })
+})
