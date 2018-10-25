6269b3bf11af207569f0b90d7b7aa11e84e11cd5
totemteleko <totemteleko@gmail.com>
2018-10-25 16:23:41 -0400

    Check if unique

--- palindrome_test.go
+++ palindrome_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 var _ = Describe("Palindrome", func() {
-  FIt("checks correct palindromes", func(){
+  It("checks correct palindromes", func(){
     Expect(CheckPalindrome("ov")).To(BeFalse())    
     Expect(CheckPalindrome("ovo")).To(BeTrue())    
     Expect(CheckPalindrome("abba")).To(BeTrue())    
---
+++ uniquecharacters_test.go
@@ -0,0 +1,16 @@
+package algorithms_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/jszroberto/go-algorithms"
+)
+
+var _ = Describe("Uniquecharacters", func() {
+  It("checks if unique", func(){
+    Expect(IsUnique("ab")).To(BeTrue())
+    Expect(IsUnique("abb")).To(BeFalse())
+    Expect(IsUnique("")).To(BeTrue())
+  })
+})
