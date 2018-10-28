1b637f0fd61c410d16616ac8360bb6ca9874fd9a
Roberto Jimenez <jszroberto@gmail.com>
2018-10-28 13:09:39 +0100

    Add find in rotated array iterative

--- search/findinrotatedarray.go
+++ search/findinrotatedarray.go
@@ -1,48 +1,48 @@
 package search
 
-type RotatedArray []int
-
-func (a RotatedArray) IsSorted(i, j int) bool {
-	if i == j {
-		return true
+func couldContain(v int, arr []int, i, j int) bool {
+	if arr[i] < arr[j] {
+		return v >= arr[i] && v <= arr[j]
 	}
-	return a[j] > a[i]
+	return v <= arr[i] && v >= arr[j]
 }
 
-func (a RotatedArray) MayContain(v, i, j int) bool {
-	if i == j {
-		return v == a[i]
-	}
-	return a[j] >= v && v >= a[i]
-}
-
-func (a RotatedArray) IsValid(i, j int) bool {
-	return i >= 0 && j < len(a) && i <= j
-}
-
-func findInRotatedArray(v int, arr RotatedArray, start, end int) int {
-	mid := (start + end) / 2
-
-	if arr[mid] == v {
-		return mid
+func FindInRotatedArray(v int, arr []int) int {
+	if len(arr) == 0 {
+		return -1
 	}
 
-	if arr.IsValid(start, mid-1) && arr.IsSorted(start, mid-1) {
-		if arr.MayContain(v, start, mid-1) {
-			return findInRotatedArray(v, arr, start, mid-1)
+	left, right := 0, len(arr)-1
+	for left <= right {
+		mid := (right + left) / 2
+		if arr[mid] == v {
+			return mid
 		}
-		return findInRotatedArray(v, arr, mid+1, end)
-	}
 
-	if arr.IsValid(start, mid+1) && arr.IsSorted(mid+1, end) {
-		if arr.MayContain(v, mid+1, end) {
-			return findInRotatedArray(v, arr, mid+1, end)
+		if couldContain(v, arr, left, mid-1) {
+			right = mid - 1
+		} else {
+			left = mid + 1
 		}
-		return findInRotatedArray(v, arr, start, mid-1)
 	}
 	return -1
 }
 
-func FindInRotatedArray(v int, arr []int) int {
-	return findInRotatedArray(v, RotatedArray(arr), 0, len(arr)-1)
+func findInRotateArrayRecursive(v int, arr []int, left, right int) int {
+	if len(arr) == 0 || left > right {
+		return -1
+	}
+
+	mid := (right + left) / 2
+	if arr[mid] == v {
+		return mid
+	}
+	if couldContain(v, arr, left, mid-1) {
+		return findInRotateArrayRecursive(v, arr, left, mid-1)
+	}
+	return findInRotateArrayRecursive(v, arr, mid+1, right)
+}
+
+func FindInRotatedArrayRecursive(v int, arr []int) int {
+	return findInRotateArrayRecursive(v, arr, 0, len(arr)-1)
 }
--- search/findinrotatedarray_test.go
+++ search/findinrotatedarray_test.go
@@ -17,5 +17,20 @@ var _ = Describe("Findinrotatedarray", func() {
 		Expect(FindInRotatedArray(7, arr)).To(Equal(4))
 		Expect(FindInRotatedArray(1, arr)).To(Equal(5))
 		Expect(FindInRotatedArray(2, arr)).To(Equal(6))
+		Expect(FindInRotatedArray(0, arr)).To(Equal(-1))
+		Expect(FindInRotatedArray(8, arr)).To(Equal(-1))
+	})
+
+	It("works with recursive", func() {
+		arr := []int{3, 4, 5, 6, 7, 1, 2}
+		Expect(FindInRotatedArrayRecursive(3, arr)).To(Equal(0))
+		Expect(FindInRotatedArrayRecursive(4, arr)).To(Equal(1))
+		Expect(FindInRotatedArrayRecursive(5, arr)).To(Equal(2))
+		Expect(FindInRotatedArrayRecursive(6, arr)).To(Equal(3))
+		Expect(FindInRotatedArrayRecursive(7, arr)).To(Equal(4))
+		Expect(FindInRotatedArrayRecursive(1, arr)).To(Equal(5))
+		Expect(FindInRotatedArrayRecursive(2, arr)).To(Equal(6))
+		Expect(FindInRotatedArray(0, arr)).To(Equal(-1))
+		Expect(FindInRotatedArray(8, arr)).To(Equal(-1))
 	})
 })
